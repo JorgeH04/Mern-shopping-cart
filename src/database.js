b08b192e4678f1247f3feea36303c67a5dd2aafd
Jorge <jhessle04@gmail.com>
2020-04-10 02:26:01 -0300

    n

--- src/database.js
+++ src/database.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
-const { database } = require('./keys');
+//const mongoose = require('mongoose');
+//const { database } = require('./keys');
 
 
-mongoose.connect(database.URI,  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
- .then(db => console.log('DB is connected'))
- .catch(err => console.error(err));
+//mongoose.connect(database.URI,  {
+ //   useNewUrlParser: true,
+  //  useUnifiedTopology: true
+//})
+ //.then(db => console.log('DB is connected'))
+ //.catch(err => console.error(err));
+
+
+ const mongoose = require('mongoose');
+
+  mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true 
+  }) 
+      .then(db => console.log(`DB is connected`))
+      .catch(err => console.error(err));  
